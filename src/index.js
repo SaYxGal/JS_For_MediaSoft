4a28f3b68e4d096509058b36ecb122ba6f8f6bb4
Danil Malin <malin-03@mail.ru>
2022-08-04 15:24:18 +0400

    Добавлен json-server и получение данных из data.json. Для реализации также использовался axios

--- src/index.js
+++ src/index.js
@@ -6,62 +6,63 @@ import ModalWindow from './components/modalwindow'
 import 'normalize.css'
 import './index.scss'
 
-const mocks = [
-    {
-        "id": 1,
-        "name": "Ноутбук 1",
-        "price": 4500,
-        "picture": 1
-    },
-    {
-        "id": 2,
-        "name": "Игровая консоль",
-        "price": 7000,
-        "picture": 4
-    },
-    {
-        "id": 3,
-        "name": "Смартфон 1",
-        "price": 3000,
-        "picture": 3
-    },
-    {
-        "id": 4,
-        "name": "Блокнот",
-        "price": 60,
-        "picture": 2
-    },
-    {
-        "id": 5,
-        "name": "Ноутбук 2",
-        "price": 8000,
-        "picture": 1
-    },
-    {
-        "id": 6,
-        "name": "Смартфон 2",
-        "price": 5500,
-        "picture": 3
-    },
-    {
-        "id": 7,
-        "name": "Смартфон (улучшенный)",
-        "price": 9000,
-        "picture": 3
-    },
-    {
-        "id": 8,
-        "name": "Игровая консоль с периферией",
-        "price": 10000,
-        "picture": 4
-    },
-    {
-        "id": 9,
-        "name": "Ноутбук (БУ)",
-        "price": 1000,
-        "picture": 1
-    }
-]
+// const mocks = [ Для того, чтобы не использовать json-server, надо расскомментировать эту часть, закомментировать второй mocks и функцию getData и вызвать generateItems самому
+//     {
+//         "id": 1,
+//         "name": "Ноутбук 1",
+//         "price": 4500,
+//         "picture": 1
+//     },
+//     {
+//         "id": 2,
+//         "name": "Игровая консоль",
+//         "price": 7000,
+//         "picture": 4
+//     },
+//     {
+//         "id": 3,
+//         "name": "Смартфон 1",
+//         "price": 3000,
+//         "picture": 3
+//     },
+//     {
+//         "id": 4,
+//         "name": "Блокнот",
+//         "price": 60,
+//         "picture": 2
+//     },
+//     {
+//         "id": 5,
+//         "name": "Ноутбук 2",
+//         "price": 8000,
+//         "picture": 1
+//     },
+//     {
+//         "id": 6,
+//         "name": "Смартфон 2",
+//         "price": 5500,
+//         "picture": 3
+//     },
+//     {
+//         "id": 7,
+//         "name": "Смартфон (улучшенный)",
+//         "price": 9000,
+//         "picture": 3
+//     },
+//     {
+//         "id": 8,
+//         "name": "Игровая консоль с периферией",
+//         "price": 10000,
+//         "picture": 4
+//     },
+//     {
+//         "id": 9,
+//         "name": "Ноутбук (БУ)",
+//         "price": 1000,
+//         "picture": 1
+//     }
+// ]
+let mocks = []
 const generateItems = (needFilter, typeOfSort) => {
     let child = ItemListElement.childNodes[0].lastElementChild
     while(child){
@@ -85,7 +86,7 @@ const generateItems = (needFilter, typeOfSort) => {
             }
         }
         catch(err){
-            console.log(err.message)
+            alert(err.message)
         }
     })
     for(let i = 0; i < ItemListElement.childNodes[0].childNodes.length; i = i + 1){
@@ -105,11 +106,22 @@ const generateItems = (needFilter, typeOfSort) => {
         }
     }
 }
+async function getData(){
+    const axios = require('axios')
+    try{
+        const response = await axios.get('http://localhost:3000/items')
+        mocks = response.data
+        generateItems(false, 0);
+    }
+    catch(err){
+        alert(err.message)
+    }
+}
 let cart = []
 const {ItemListElement, FiltFunction} = ItemsList(generateItems)
 const ModalElement = ModalWindow()
 const {CartElement, updateCart} = Cart(cart, ModalElement)
 document.body.appendChild(ItemListElement)
-generateItems(false, 0);
+getData()
 document.body.appendChild(CartElement)
 document.body.appendChild(ModalElement)
